ca4a0df1e6bde511b96d909adfc98d4401ed00a8
ohshyuk5 <seunghyukoh0@gmail.com>
2020-02-07 15:08:20 +0900

    Some changes to check whether the save/load modules work

--- dqn/train_example.py
+++ dqn/train_example.py
@@ -25,16 +25,17 @@ def main(args):
         exploration_final_eps=0.02,
         model_path='cartpole_model.zip'
     )
-    # model.load(load_path='cartpole_model.zip')
+    model.load(load_path='cartpole_model.zip')
     model.learn(total_timesteps=args.max_timesteps)
 
-    print("Saving model to cartpole_model.zip")
+    print("\nFinished")
 
+    # print("Saving model to cartpole_model.zip")
     # model.save("cartpole_model.zip")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Train DQN on cartpole")
-    parser.add_argument('--max-timesteps', default=1000000, type=int, help="Maximum number of timesteps")
+    parser.add_argument('--max-timesteps', default=10000, type=int, help="Maximum number of timesteps")
     args = parser.parse_args()
     main(args)
