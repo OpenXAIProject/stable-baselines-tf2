02442226c6b2b3729c8772e2903963ad590313f7
ohshyuk5 <5sEunghyuk!>
2020-03-02 11:12:24 +0900

    Train atari model

--- dqn/train_atari_Breakout.py
+++ dqn/train_atari_Breakout.py
@@ -33,56 +33,32 @@ def main():
     parser.add_argument('--prioritized', type=int, default=1)
     parser.add_argument('--dueling', type=int, default=1)
     parser.add_argument('--prioritized-replay-alpha', type=float, default=0.6)
-    parser.add_argument('--num-timesteps', type=int, default=int(10e6))
+    parser.add_argument('--num-timesteps', type=int, default=int(1e7))
 
     args = parser.parse_args()
     logger.configure()
     set_global_seeds(args.seed)
-    Vec = True
-    if not Vec:
-        env = make_atari(args.env)
-        env = bench.Monitor(env, logger.get_dir())
-        env = wrap_atari_dqn(env)
-    else:
-        # venv = DummyVecEnv(np.array([env, _env]))
-        venv = SubprocVecEnv([make_atari])
-        venv = VecFrameStack(venv, 4)
 
-    policy = partial(CnnPolicy, dueling=args.dueling == 1)
+    env = make_atari(args.env)
+    env.action_space.seed(args.seed)
+    env = bench.Monitor(env, logger.get_dir())
+    env = wrap_atari_dqn(env)
 
-    # model = DQN(
-    #     env=env,
-    #     policy=policy,
-    #     learning_rate=1e-4,
-    #     buffer_size=10000,
-    #     exploration_fraction=0.1,
-    #     exploration_final_eps=0.01,
-    #     train_freq=4,
-    #     learning_starts=10000,
-    #     target_network_update_freq=1000,
-    #     gamma=0.99,
-    #     prioritized_replay=bool(args.prioritized),
-    #     prioritized_replay_alpha=args.prioritized_replay_alpha,
-    # )
     model = DQN(
-        env=venv,
+        env=env,
         policy_class=CnnPolicy,
-        learning_rate=1e-4,
         buffer_size=10000,
-        double_q=False,
-        prioritized_replay=True,
-        dueling=True,
-        train_freq=4,
+        learning_rate=1e-4,
         learning_starts=10000,
-        exploration_fraction=0.1,
-        exploration_final_eps=0.01,
         target_network_update_freq=1000,
-        model_path='atari_test'
+        train_freq=4,
+        exploration_final_eps=0.01,
+        exploration_fraction=0.1,
+        prioritized_replay=True,
+        model_path='atari_test_Breakout'
     )
-    # model.load('atari_Breakout_duel')
-    # model.evaluate(num_epsiodes=50)
     model.learn(total_timesteps=args.num_timesteps)
-    venv.close()
+    env.close()
 
 
 if __name__ == '__main__':
